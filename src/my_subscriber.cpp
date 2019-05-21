6aff2ac74c375bf9c78439ee009e4b2749a1c33d
Johanna Gleichauf <johanna.gleichauf@th-nuernberg.de>
2019-05-21 12:41:22 +0200

    Added camera_info

--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -7,6 +7,12 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <camera_info_manager/camera_info_manager.h>
+#include <sensor_msgs/CameraInfo.h>
+
+sensor_msgs::CameraInfo                 _camera_info;
+image_transport::CameraPublisher*       _camera_info_pub     = NULL;
+camera_info_manager::CameraInfoManager* _camera_info_manager = NULL;
 
 void callback(const sensor_msgs::ImageConstPtr& image)
 {
@@ -41,11 +47,47 @@ sensor_msgs::Image toImageMsg(const Cvb::ImagePtr& cvbImg)
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "something");
-  ros::NodeHandle nh;
-  image_transport::ImageTransport it(nh);
+  ros::NodeHandle nh_("~");
+  ros::NodeHandle n;
+  image_transport::ImageTransport it(n);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
     image_transport::Subscriber sub = it.subscribe("camera/image", 1, callback);
+
+    std::string camera_name;
+     std::string camera_info_url;
+     nh_.getParam("camera_name", camera_name);
+     nh_.getParam("camera_info_url", camera_info_url);
+
+     // initialize CameraInfoManager, providing set_camera_info service for geometric calibration
+     // see http://wiki.ros.org/camera_info_manager
+     camera_info_manager::CameraInfoManager cinfo_manager(n);
+     _camera_info_manager = &cinfo_manager;
+
+     if (!_camera_info_manager->setCameraName(camera_name))
+     {
+       // GUID is 16 hex digits, which should be valid.
+       // If not, use it for log messages anyway.
+       ROS_WARN_STREAM("[" << camera_name << "] name not valid" << " for camera_info_manger");
+     }
+
+     if (_camera_info_manager->validateURL(camera_info_url))
+     {
+       if ( !_camera_info_manager->loadCameraInfo(camera_info_url) )
+       {
+         ROS_WARN( "camera_info_url does not contain calibration data." );
+       }
+       else if ( !_camera_info_manager->isCalibrated() )
+       {
+         ROS_WARN( "Camera is not calibrated. Using default values." );
+       }
+     }
+     else
+     {
+       ROS_ERROR_STREAM_ONCE( "Calibration URL syntax is not supported by CameraInfoManager." );
+     }
+
+
 
   try
   {
@@ -54,6 +96,7 @@ std::cout << " Try " << std::endl;
     stream->Start();
 
    sensor_msgs::Image image;
+   sensor_msgs::Image imageRos;
    cv_bridge::CvImagePtr frame;
    cv::Mat Input;
    cv::Mat cropped;
@@ -74,8 +117,12 @@ std::cout << " Try " << std::endl;
     	cv_bridge::CvImage resizeRos;
     	resizeRos.encoding = "rgb8";
     	resizeRos.image = Output;
+//    	resizeRos.toImageMsg(imageRos);
     	sensor_msgs::ImagePtr imagePtr = resizeRos.toImageMsg();
     	imagePtr->header.stamp = ros::Time::now();
+//    	_camera_info = _camera_info_manager->getCameraInfo();
+//    	_camera_info.header = imageRos.header;
+//    	_camera_info_pub->publish(imageRos, _camera_info);
     	pub.publish(imagePtr);
 //        pub.publish(toImageMsg(waitResult.Image));
         ros::spinOnce();
